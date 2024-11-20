01aef318e8420ff86cf744ce202457889de0246d
Guilherme Kohl <73613460+g-kohl@users.noreply.github.com>
2024-11-20 20:38:59 -0300

    Update image.hpp

--- image.hpp
+++ image.hpp
@@ -2,9 +2,9 @@
 
 #include <iostream>
 #include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
 #include <QVector>
 
-#include "/usr/local/include/opencv4/opencv2/opencv.hpp"
 
 class Image {
 public:
